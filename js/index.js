98dd9c27118e13cfd7bb634cb737f7d10a80d45f
guichao2019 <605736118@qq.com>
2019-03-28 23:40:29 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    // let box1 = document.getElementsByClassName("imgbox")[0]
+	// let box2 = document.getElementsByClassName("i")[0]
+	// // box2.style.display = "none"
+	// box1.onmouseover = function(){
+	//     box2.style.display = "block"
+	// }
+	// box1.onmouseout = function(){
+	//     box2.style.display = "none"
+	// }
+}
